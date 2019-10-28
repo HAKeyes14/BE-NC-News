61aad59a3002e0314fb6d9c5ff538f6e63765646
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-10-28 11:31:40 +0000

    created formatDates function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,12 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+    const formattedArr = list.map(obj => {
+        const formattedObj = {...obj};
+        const formattedDate = new Date(obj.created_at);
+        formattedObj.created_at = formattedDate;
+        return formattedObj;
+    });
+    return formattedArr;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,7 +5,138 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('returns an empty array when passed an empty array', () => {
+    expect(formatDates([])).to.eql([]);
+  });
+  it('when passed an array with a single object, returns an array with that objects date formatted', () => {
+    const input = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }];
+    const expected = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: new Date(1542284514171),
+      votes: 100,
+    }];
+    expect(formatDates(input)).to.eql(expected);
+  });
+  it('the returned array is not the passed array', () => {
+    const input = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }];
+    expect(formatDates(input)).to.not.equal(input);
+  });
+  it('the passed array is not mutated', () => {
+    const input = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }];
+    const copy = [];
+    input.forEach(obj => {
+      copy.push({...obj});
+    })
+    formatDates(input);
+    expect(input).to.eql(copy);
+  });
+  it('returns an array of many formatted objects when passed an array of many objects', () => {
+    const input = [
+      {
+        title: 'Living in the shadow of a great man',
+        topic: 'mitch',
+        author: 'butter_bridge',
+        body: 'I find this existence challenging',
+        created_at: 1542284514171,
+        votes: 100,
+      },
+      {
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body:
+          'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+        created_at: 1416140514171,
+      },
+      {
+        title: 'Eight pug gifs that remind me of mitch',
+        topic: 'mitch',
+        author: 'icellusedkars',
+        body: 'some gifs',
+        created_at: 1289996514171,
+      },
+      {
+        title: 'Student SUES Mitch!',
+        topic: 'mitch',
+        author: 'rogersop',
+        body:
+          'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+        created_at: 1163852514171,
+      },
+      {
+        title: 'UNCOVERED: catspiracy to bring down democracy',
+        topic: 'cats',
+        author: 'rogersop',
+        body: 'Bastet walks amongst us, and the cats are taking arms!',
+        created_at: 1037708514171,
+      }];
+      const expected = [
+        {
+          title: 'Living in the shadow of a great man',
+          topic: 'mitch',
+          author: 'butter_bridge',
+          body: 'I find this existence challenging',
+          created_at: new Date(1542284514171),
+          votes: 100,
+        },
+        {
+          title: 'Sony Vaio; or, The Laptop',
+          topic: 'mitch',
+          author: 'icellusedkars',
+          body:
+            'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+          created_at: new Date(1416140514171),
+        },
+        {
+          title: 'Eight pug gifs that remind me of mitch',
+          topic: 'mitch',
+          author: 'icellusedkars',
+          body: 'some gifs',
+          created_at: new Date(1289996514171),
+        },
+        {
+          title: 'Student SUES Mitch!',
+          topic: 'mitch',
+          author: 'rogersop',
+          body:
+            'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+          created_at: new Date(1163852514171),
+        },
+        {
+          title: 'UNCOVERED: catspiracy to bring down democracy',
+          topic: 'cats',
+          author: 'rogersop',
+          body: 'Bastet walks amongst us, and the cats are taking arms!',
+          created_at: new Date(1037708514171),
+        }];
+        expect(formatDates(input)).to.eql(expected);
+  });
+});
 
 describe('makeRefObj', () => {});
 
