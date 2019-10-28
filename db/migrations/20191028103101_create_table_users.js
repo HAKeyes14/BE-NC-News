5cfe4e8cb1e24cbdf86e2ea90a91935dd70e73e0
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-10-28 13:53:55 +0000

    added .unique to username and slug in migrations

--- db/migrations/20191028103101_create_table_users.js
+++ db/migrations/20191028103101_create_table_users.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('users', usersTable => {
-    usersTable.string('username').primary();
+    usersTable.string('username').primary().unique();
     usersTable.string('avatar_url').notNullable();
     usersTable.string('name').notNullable();
   });
