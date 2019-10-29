45a7e1872ce07a5c14375a3328d9a8a675d441e2
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-10-29 10:47:28 +0000

    changed error messages for user endpoints

--- models/users.js
+++ models/users.js
@@ -4,7 +4,7 @@ exports.selectUserByUsername = (username) => {
     if (username.length > 20) {
         return Promise.reject({
             status: 400,
-            message: `400 - Username: ${username} is not a valid username.`,
+            message: `Username: ${username} is not a valid username.`,
         });
     }
     return connection('users')
@@ -14,7 +14,7 @@ exports.selectUserByUsername = (username) => {
         if (!user) {
             return Promise.reject({
                 status: 404,
-                message: `404 - User with username: ${username} does not exist.`,
+                message: `User with username: ${username} does not exist.`,
             });
         }
         return user;
