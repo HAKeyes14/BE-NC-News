67254366741c170dbda4dd0a49467cdf738bca7a
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-11-15 09:26:39 +0000

    hardcoded token for hosting

--- controllers/login.js
+++ controllers/login.js
@@ -2,7 +2,7 @@ const {selectUserByUsername} = require('../models/login');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET || require('../config').JWT_SECRET;
 const bcrypt = require('bcrypt');
-console.log(JWT_SECRET)
+
 exports.sendToken = (req, res, next) => {
     const {username, password} = req.body;
     selectUserByUsername(username)
@@ -12,11 +12,11 @@ exports.sendToken = (req, res, next) => {
     })
     .then(([passwordOk, user]) => {
         if (user && passwordOk) {
-            const token = jwt.sign(
-                { user: user.username, iat: Date.now() },
-                JWT_SECRET
-            );
-            res.send({ token });
+            // const token = jwt.sign(
+            //     { user: user.username, iat: Date.now() },
+            //     JWT_SECRET
+            // );
+            res.send({ token: 'test' });
         } else {
             next({ status: 401, message: 'Invalid username or password.' });
         }
@@ -26,11 +26,12 @@ exports.sendToken = (req, res, next) => {
 
 exports.authorise = (req, res, next) => {
     const { authorization } = req.headers;
-    console.log(req)
-    console.log(req.headers)
     const token = authorization.split(' ')[1];
-    jwt.verify(token, JWT_SECRET, (err, res) => {
-        if (err) next({ status: 401, message: 'Unauthorised' });
-        else next();
-    })
+    if (token !== "test") {
+        next({ status: 401, message: 'Unauthorised' });
+    } else next();
+    // jwt.verify(token, JWT_SECRET, (err, res) => {
+    //     if (err) next({ status: 401, message: 'Unauthorised' });
+    //     else next();
+    // })
 };
