ab5ac4a00404f13008efe016a9ab5bfdf1775bcd
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-11-18 13:59:34 +0000

    installed cors

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const apiRouter = require('./routes/api-router')
-const {customErrorHandler, psqlErrorHandler, serverErrorHandler, routeNotFoundErrorHandler} = require('./error-handling/error-handlers');
-const {sendToken, authorise} = require('./controllers/login');
+const express = require("express");
+const apiRouter = require("./routes/api-router");
+const {
+  customErrorHandler,
+  psqlErrorHandler,
+  serverErrorHandler,
+  routeNotFoundErrorHandler
+} = require("./error-handling/error-handlers");
+const { sendToken, authorise } = require("./controllers/login");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/login', sendToken);
+app.use("/api/login", sendToken);
 app.use(authorise);
-app.use('/api', apiRouter);
-app.all('/*', routeNotFoundErrorHandler);
+app.use("/api", apiRouter);
+app.all("/*", routeNotFoundErrorHandler);
 app.use(customErrorHandler);
 app.use(psqlErrorHandler);
 app.use(serverErrorHandler);
--- error-handling/error-handlers.js
+++ error-handling/error-handlers.js
@@ -1,50 +1,48 @@
-const createMessage = (err) => {
-    return err.message.split(' - ')[1];
-}
+const createMessage = err => {
+  return err.message.split(" - ")[1];
+};
 
 exports.customErrorHandler = (err, req, res, next) => {
-    if(err.status) {
-        res.status(err.status).send({msg: err.message});
-    } else next(err);
-}
+  if (err.status) {
+    res.status(err.status).send({ msg: err.message });
+  } else next(err);
+};
 
 exports.psqlErrorHandler = (err, req, res, next) => {
-    const psqlCodes = {
-        '22P02': {
-            status: 400,
-            msg: createMessage(err)
-        },
-        '23503': {
-            status: 404,
-            msg: createMessage(err)
-        },
-        '23502': {
-            status: 400,
-            msg: createMessage(err)
-        },
-        '42703': {
-            status: 400,
-            msg: createMessage(err)
-        }
+  const psqlCodes = {
+    "22P02": {
+      status: 400,
+      msg: createMessage(err)
+    },
+    "23503": {
+      status: 404,
+      msg: createMessage(err)
+    },
+    "23502": {
+      status: 400,
+      msg: createMessage(err)
+    },
+    "42703": {
+      status: 400,
+      msg: createMessage(err)
     }
+  };
 
-    const thisError = psqlCodes[err.code];
-    
-    if (thisError) {
-        res.status(thisError.status).send({msg: thisError.msg});
-    } else next(err);
+  const thisError = psqlCodes[err.code];
 
-}
+  if (thisError) {
+    res.status(thisError.status).send({ msg: thisError.msg });
+  } else next(err);
+};
 
 exports.serverErrorHandler = (err, req, res, next) => {
-    res.status(500).send({msg: "something is wrong!"});
-}
+  res.status(500).send({ msg: "something is wrong!" });
+};
 
 exports.send405Error = (req, res, next) => {
-    res.status(405).send({ msg: 'method not allowed' });
+  res.status(405).send({ msg: "method not allowed" });
 };
 
 exports.routeNotFoundErrorHandler = (req, res, next) => {
-    res.status(404).send({msg: `Route "${req.originalUrl}" not found`});
-}
-  
+  res.status(404).send({ msg: `Route "${req.originalUrl}" not found` });
+};
